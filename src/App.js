89a27c081f1ffc71269e659a619c9a31d578f218
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-04-18 13:13:32 +0200

    frontend packages installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from './Screens/HomeScreen';
+import CreateScreen from './Screens/CreateScreen';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomeScreen />} />
+        <Route path='/create' element={<CreateScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import './bootstrap.min.css' // stil leri hazir aldim. bootswatch.com'dan. theme Sketchy
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
