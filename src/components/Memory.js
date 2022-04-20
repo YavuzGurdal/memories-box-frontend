4702f16bac76056ceb5a91b733aba437e57fad83
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-04-20 19:33:08 +0200

    memory component added

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import moment from 'moment'
+import { Card } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { MdModeEdit, MdDelete } from 'react-icons/md';
 
 const Memory = ({ memory }) => { // HomeScreen den props la gonderilen memory'yi yakaliyorum
     return (
-        <div>
-            <h1>{memory.title}</h1>
-        </div>
+        <>
+            <Card className='rounded pb-3 my-3' style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={memory.image} />
+                <Card.Body>
+                    <Card.Title style={{ color: 'darkblue' }} className='text-capitalize' >{memory.title}</Card.Title>
+                    <Card.Text style={{ letterSpacing: '1px' }}>
+                        {memory.content}
+                    </Card.Text>
+                    <Card.Title className='text-capitalize' ><span style={{ color: 'darkblue' }}>Creator: </span>{memory.creator}</Card.Title>
+                    <Card.Subtitle>{moment(memory.createdAt).fromNow()}</Card.Subtitle>
+                </Card.Body>
+                <Card.Footer className='bg-white pb-0 d-flex justify-content-between'>
+                    <LinkContainer to='/' style={{ cursor: 'pointer' }}>
+                        <MdModeEdit size={25} color='#0d6efd' />
+                    </LinkContainer>
+                    <MdDelete style={{ cursor: 'pointer' }} size={25} color='#dc3545' />
+                </Card.Footer>
+            </Card>
+        </>
     )
 }
 
