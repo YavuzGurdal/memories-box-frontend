a357abe2d45596ad20ff197f9bfd6fbbf490b0a1
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-04-21 17:49:31 +0200

    redux installed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter } from "react-router-dom";
 import './bootstrap.min.css' // stil leri hazir aldim. bootswatch.com'dan. theme Sketchy
 
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import memoriesReducer from './reducers/memoriesReducer'; // reducer icindeki memories'i bu guncelleyecek
+
+const reducer = combineReducers({ // tum reducerlari burda birlestiriyorum. yani combine ediyorum. sonra da store icine gondermis oluyorum
+  memories: memoriesReducer  // bu global memories
+})
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk))) // bunlar her redux kurulumunda benzer sekilde oluyor
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// App i provider ile sariyorum ve store a yukarda tanimladigim store u atiyorum
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
