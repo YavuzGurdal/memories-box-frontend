af4538264f8690a1482481a37fc85196f9494af9
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-05-16 19:36:53 +0200

    deployment

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({ // bunu surekli ayni seyi yazmamak icin yazdim
-    baseURL: 'http://localhost:8080',
+    //baseURL: 'http://localhost:8080',
+    baseURL: 'https://memories-boxx.herokuapp.com/',
     withCredentials: true, // cookie icin
 })
 
